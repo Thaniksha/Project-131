043a55683c78ca458485c44c540263c70059acbd
Thaniksha <73345437+Thaniksha@users.noreply.github.com>
2022-06-04 15:49:48 +0530

    Add files via upload

--- tv.js
+++ tv.js
@@ -3,14 +3,14 @@ img="";
 object_detector="";
 
 function preload(){
-    img=loadImage('bed_room.jpg');
+    img=loadImage('tv.webp');
 }
 
 function setup(){
     canvas=createCanvas(640,350);
     canvas.position(350,200);
     object_detector=ml5.objectDetector('cocossd',modelLoaded);
-    document.getElementById("status").innerHTML="Status : Detecting Objects";
+    document.getElementById("status4").innerHTML="Status : Detecting Objects";
 }
 
  function modelLoaded(){
