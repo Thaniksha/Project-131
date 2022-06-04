2488f78a8b3e68b618831577202e568e8365fd6a
Thaniksha <73345437+Thaniksha@users.noreply.github.com>
2022-06-04 20:37:26 +0530

    Add files via upload

--- bed_room.js
+++ bed_room.js
@@ -25,9 +25,23 @@ function setup(){
          console.error(error);
      }
      console.log(results);
+     objects=results;
  }
 
  function  draw(){
      image(img,0,0,640,350);
-     
- }
+     if (status != "") {
+  	  image(img, 0, 0, 640, 420);
+    for (i = 0; i < objects.length; i++) {
+      document.getElementById("status").innerHTML = "Status : Objects Detected";
+
+      fill(255, 0, 0);
+      percent = floor(objects[i].confidence * 100);
+      text(objects[i].label + " " + percent + "%", objects[i].x + 5, objects[i].y + 15);
+      noFill();
+      stroke(255, 0, 0);
+      rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+    }
+  }
+}
+ 
